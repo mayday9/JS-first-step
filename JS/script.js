87bc014823bc33e090c02455f050e861866afeb5
mayday9 <themaydaygmt@gmail.com>
2018-06-02 11:02:30 +0300

    Using "while" to finish HomeWork

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,16 @@
-var fio, sex, age, e_mail;
+var fio, sex, age, e_mail, result=false;
 
-do {
+
+while(result==false){
     fio = prompt("Введите фамилию, имя, отчество",);
     sex = prompt("Введите ваш пол",);
     age = parseInt(prompt("Введите ваш возраст",));
     e_mail = prompt("Введите ваш e-mail",);
-}
 
-while(confirm(("Ваше фио: "+fio+", "+"Ваш пол: "+sex+", "+"Ваш возраст: "+age+", "+"Ваш e-mail: "+e_mail+"."), "Все верно?" ))
-      alert("Спасибо за инфо!")
+
+    result=confirm("Ваше фио: "+fio+".\n"+"Ваш пол: "+sex+".\n"+"Ваш возраст: "+age+".\n"+"Ваш e-mail: "+e_mail+".\n"+"Все верно?")
+}
+alert("Спасибо за инфо!")
 
 
 
